2491bad45983cd5117b10790fd7923d329b4ab00
Vishwanath Waghmare <102869329+vishwanathwaghmare@users.noreply.github.com>
2023-11-02 19:12:28 +0530

    Add files via upload

---
+++ MultithreadingDemo/MultithreadingDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultithreadingDemo
+{
+    public class ThreadDemo
+    {
+        public void First()
+        {
+            for(int i=1; i<=5; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(2000);
+            }
+        }
+        public void Second()
+        {
+            for (int i = 6; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(2000);
+
+            }
+        }
+
+
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ThreadDemo first = new ThreadDemo();
+            //create thread
+            // allocate method ref to the ThreadStart delegate
+            Thread t1 = new Thread(new ThreadStart(first.First));
+            Thread t2 = new Thread(new ThreadStart(first.Second));
+            t2.Priority = ThreadPriority.Highest;
+            t1.Priority = ThreadPriority.AboveNormal;
+            t1.Start();
+            //t1.Join();
+            t1.Join(2000);
+            t2.Start();
+
+        }
+    }
+}
